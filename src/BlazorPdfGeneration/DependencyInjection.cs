4c24040010771d6e5cd571f5eeb0bb6964d6c037
BlazorPdfGeneration <blazorpdfgeneration@outlook.com>
2024-05-29 14:08:06 +1000

    Initial commit

---
+++ src/BlazorPdfGeneration/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorPdfGeneration;
+
+internal static class DependencyInjection
+{
+	internal static IServiceCollection AddInfrastructure(this IServiceCollection services)
+	{
+		return services;
+	}
+}
